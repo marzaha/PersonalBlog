ab1df68cd48b213de912532fb96d16e25c63d494
lofts <gb_2312@126.com>
2018-11-01 15:26:01 +0800

    modify file format

--- src/com/lofts/blog/service/RegisterServlet.java
+++ src/com/lofts/blog/service/RegisterServlet.java
@@ -22,14 +22,12 @@ public class RegisterServlet extends HttpServlet {
 	}
 
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doPost(req, resp);
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
 		resp.setContentType("text/html;charset=utf-8");
 
@@ -37,17 +35,17 @@ public class RegisterServlet extends HttpServlet {
 		String name = req.getParameter("username");
 		String password = req.getParameter("password");
 		int role = Integer.valueOf(req.getParameter("role"));
-		
-		User user=new User();
-                user.setId(id);
-                user.setName(name);
-                user.setPassword(password);
-                user.setRole(role);
-                UserDao userDAO=new UserDao(); 
-                userDAO.addUser(user);
-        
-                System.out.println("注册成功");
-                req.getRequestDispatcher("login.jsp").forward(req, resp);
+
+		User user = new User();
+		user.setId(id);
+		user.setName(name);
+		user.setPassword(password);
+		user.setRole(role);
+		UserDao userDAO = new UserDao();
+		userDAO.addUser(user);
+
+		System.out.println("注册成功");
+		req.getRequestDispatcher("login.jsp").forward(req, resp);
 
 	}
 
