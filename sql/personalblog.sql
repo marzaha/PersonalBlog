c6d0fdc5447d865c63b06d6d1bdbc78c96db5c70
lofts <gb_2312@126.com>
2018-11-05 16:34:54 +0800

    modify databse

--- sql/personalblog.sql
+++ sql/personalblog.sql
@@ -10,25 +10,47 @@ Target Server Type    : MYSQL
 Target Server Version : 50622
 File Encoding         : 65001
 
-Date: 2018-11-01 17:03:08
+Date: 2018-11-05 16:30:48
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for diary
+-- ----------------------------
+DROP TABLE IF EXISTS `diary`;
+CREATE TABLE `diary` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `createdate` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of diary
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `role` int(10) DEFAULT NULL,
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `headimage` varchar(255) DEFAULT NULL,
+  `nickname` varchar(50) DEFAULT NULL,
+  `sex` int(10) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `constellation` varchar(50) DEFAULT NULL,
+  `hobby` varchar(50) DEFAULT NULL,
+  `registerdate` date DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('8', 'jiang', '123456', '5');
-INSERT INTO `user` VALUES ('21', 'lofts', '123456', '32');
