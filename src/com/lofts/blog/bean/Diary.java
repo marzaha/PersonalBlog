02d224403a602ff5d2e9b81a735cb5a544f9014e
lofts <gb_2312@126.com>
2018-11-15 16:39:29 +0800

    add writediary

---
+++ src/com/lofts/blog/bean/Diary.java
@@ -0,0 +1,53 @@
+package com.lofts.blog.bean;
+
+public class Diary {
+
+    private int id;
+    private String title;
+    private String type;
+    private String content;
+    private String createdate;
+
+    public Diary() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedate() {
+        return createdate;
+    }
+
+    public void setCreatedate(String createdate) {
+        this.createdate = createdate;
+    }
+}
